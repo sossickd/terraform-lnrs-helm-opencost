74f76acf638f3fac23332a9b5676cede97fc12ff
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-10-24 15:20:12 +0100

    initial release

--- versions.tf
+++ versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.3.3, != 1.3.4"
 
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.34.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.9.0"
