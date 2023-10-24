f09ec8297e1997d8af664932cbd6346e3b07773c
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-10-24 10:50:06 +0100

    Add CUR report

--- aws-cur-athena.tf
+++ aws-cur-athena.tf
@@ -46,16 +46,16 @@ resource "aws_s3_bucket_ownership_controls" "cur-athena" {
 }
 
 resource "aws_cur_report_definition" "example_cur_report_definition" {
-    count = var.cloud == "aws" ? 1 : 0
-    
-    report_name = "${var.cluster_name}-cur-report"
-    time_unit = "DAILY"
-    compression = "Parquet"
-    format = "Parquet"
-    additional_schema_elements = "RESOURCES"
-    s3_bucket = aws_s3_bucket.cur-athena.id
-    s3_region = aws_s3_bucket.cur-athena.region
-    s3_prefix = "opencost-prefix/${var.cluster_name}-opencost-report"
-    additional_artifacts = "ATHENA"
-    report_versioning = "OVERWRITE_REPORT"
+  count = var.cloud == "aws" ? 1 : 0
+
+  report_name                = "${var.cluster_name}-cur-report"
+  time_unit                  = "DAILY"
+  compression                = "Parquet"
+  format                     = "Parquet"
+  additional_schema_elements = ["RESOURCES"]
+  s3_bucket                  = aws_s3_bucket.cur-athena[0].id
+  s3_region                  = aws_s3_bucket.cur-athena[0].region
+  s3_prefix                  = "opencost-prefix/${var.cluster_name}-opencost-report"
+  additional_artifacts       = ["ATHENA"]
+  report_versioning          = "OVERWRITE_REPORT"
 }
