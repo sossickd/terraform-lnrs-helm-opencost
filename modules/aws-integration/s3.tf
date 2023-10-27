73a10b2f45a1d6c83bf0a4bcdb8607570dcc10a6
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-10-27 10:41:04 +0100

    Add new aws-integration module

--- modules/aws-integration/s3.tf
+++ modules/aws-integration/s3.tf
@@ -45,28 +45,4 @@ resource "aws_s3_bucket_notification" "aws_put_s3_cur_notification" {
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "opencost-prefix/${var.cluster_name}-opencost-report/${var.cluster_name}-cur-report/${var.cluster_name}-cur-report"
   }
-
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = aws_s3_bucket.bucket.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.func1.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-    filter_suffix       = ".log"
-  }
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.func2.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "OtherLogs/"
-    filter_suffix       = ".log"
-  }
-
-  depends_on = [
-    aws_lambda_permission.allow_bucket1,
-    aws_lambda_permission.allow_bucket2,
-  ]
 }
