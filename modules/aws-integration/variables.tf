187379ef3d10e1b4f37aef380c1e2c31bbbdcb4e
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-10-27 16:07:06 +0100

    Rearrange

--- modules/aws-integration/variables.tf
+++ modules/aws-integration/variables.tf
@@ -8,7 +8,7 @@ variable "aws" {
   description = "AWS configuration."
   type = object({
     partition        = optional(string, "aws")
-    account_id       = optional(string, null)
+    account_id       = optional(string, "")
     spot_data_prefix = optional(string, "spot-datafeed")
   })
   nullable = false
