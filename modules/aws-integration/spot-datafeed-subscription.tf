c05198de06284ef3f888a6bdf996ef4730143ec0
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-10-30 12:16:08 +0000

    initial release

--- modules/aws-integration/spot-datafeed-subscription.tf
+++ modules/aws-integration/spot-datafeed-subscription.tf
@@ -1,4 +1,8 @@
 resource "aws_spot_datafeed_subscription" "spotfeed" {
   bucket = aws_s3_bucket.spotfeed.id
   prefix = var.aws.spot_data_prefix
+
+  depends_on = [
+    aws_s3_bucket.spotfeed
+  ]
 }
