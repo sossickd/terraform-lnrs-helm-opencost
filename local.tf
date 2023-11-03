cd3dec77b26e8f7e2b5e5707f947e52d1f7287de
Daniel Sossick <daniel.sossick@rbi.co.uk>
2023-11-03 12:52:13 +0000

    Change subscription id

--- local.tf
+++ local.tf
@@ -190,7 +190,7 @@ locals {
         "internetNetworkEgress": "0.143",
         "spotLabel": "kops.k8s.io/instancegroup",
         "spotLabelValue": "spotinstance-nodes",
-        "azureSubscriptionID": "${data.azurerm_subscription.subscription_id}",
+        "azureSubscriptionID": "${data.azurerm_subscription.current.subscription_id}",
         "azureStorageAccount": "${azurerm_storage_account.cost_report.name}",
         "azureStorageAccessKey": "${azurerm_storage_account.cost_report.primary_access_key}",
         "azureStorageContainer": "${azurerm_storage_container.cost_report.name}",
